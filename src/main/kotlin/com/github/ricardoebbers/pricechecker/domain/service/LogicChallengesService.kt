5d2c728f6ce78496c33ef1fc59584cb83163c195
Ricardo Ebbers <recl@cin.ufpe.br>
2020-07-19 18:23:02 -0300

    Fixed cache usage on fibonacciNthNumber Refactored fibonacciNthNumber to use BigInteger and accept higher N values

--- src/main/kotlin/com/github/ricardoebbers/pricechecker/domain/service/LogicChallengesService.kt
+++ src/main/kotlin/com/github/ricardoebbers/pricechecker/domain/service/LogicChallengesService.kt
@@ -1,9 +1,10 @@
 package com.github.ricardoebbers.pricechecker.domain.service
 
 import com.github.ricardoebbers.pricechecker.domain.enums.MoneyBill
+import java.math.BigInteger
 
 interface LogicChallengesService {
-    fun fibonacciNthNumber(n: Int): Long
+    fun fibonacciNthNumber(n: Int): BigInteger
     fun leastMoneyBillChange(changeValue: Int): Map<MoneyBill, Int>
     fun sumEvenNumbers(numbersSequence: List<Int>): Int
 }
--- src/main/kotlin/com/github/ricardoebbers/pricechecker/domain/service/impl/LogicChallengesServiceImpl.kt
+++ src/main/kotlin/com/github/ricardoebbers/pricechecker/domain/service/impl/LogicChallengesServiceImpl.kt
@@ -3,52 +3,64 @@ package com.github.ricardoebbers.pricechecker.domain.service.impl
 import com.github.ricardoebbers.pricechecker.domain.enums.MoneyBill
 import com.github.ricardoebbers.pricechecker.domain.service.LogicChallengesService
 import org.springframework.stereotype.Service
+import java.math.BigInteger
+import java.util.logging.Logger
 
 @Service
 class LogicChallengesServiceImpl : LogicChallengesService {
-    override fun fibonacciNthNumber(n: Int): Long {
-        val cache = mutableMapOf<Int, Long>()
-        memoizedRecursiveFibonacci(n, cache)
-        return cache[n] ?: 0L
+    companion object {
+        private val log = Logger.getLogger(this::class.java.simpleName)
+        private val fibonacciCache = mutableMapOf<Int, BigInteger>()
+    }
+
+    override fun fibonacciNthNumber(n: Int): BigInteger {
+        log.info("I=init, n=$n")
+        memoizedRecursiveFibonacci(n)
+        log.info("I=finish, calculated_values=$fibonacciCache, result=${fibonacciCache[n]}")
+        return fibonacciCache[n] ?: BigInteger.ZERO
+    }
+
+    private fun memoizedRecursiveFibonacci(n: Int): BigInteger {
+        return when {
+            n <= 0 -> BigInteger.ZERO.also { fibonacciCache.putIfAbsent(n, it) }
+            n <= 2 -> BigInteger.ONE.also { fibonacciCache.putIfAbsent(n, it) }
+            fibonacciCache[n] != null -> fibonacciCache.getOrDefault(n, BigInteger.ZERO)
+            else -> (memoizedRecursiveFibonacci(n - 1) +
+                    memoizedRecursiveFibonacci(n - 2)).also {
+                log.info("I=calculated, n=$n, value=$it")
+                fibonacciCache.putIfAbsent(n, it)
+            }
+        }
     }
 
     override fun leastMoneyBillChange(changeValue: Int): Map<MoneyBill, Int> {
+        log.info("I=init, changeValue=$changeValue")
         val result = mutableMapOf<MoneyBill, Int>()
-        return if (changeValue <= 0) {
-            result
+        if (changeValue <= 0) {
+            log.warning("W=returning_empty")
         } else {
-            var remainder = changeValue
-            MoneyBill.values().forEach {
-                if (remainder >= it.faceValue) {
-                    val billQuantity = remainder / it.faceValue
-                    result.merge(it, billQuantity) { old, new -> old + new }
-                    remainder -= it.faceValue * result.getOrDefault(it, 0)
-                }
+            separeIntoBills(changeValue, result)
+        }
+        log.info("I=finish, result=$result")
+        return result
+    }
+
+    private fun separeIntoBills(changeValue: Int, result: MutableMap<MoneyBill, Int>) {
+        var remainder = changeValue
+        MoneyBill.values().forEach {
+            log.info("I=calculating_for, remainder=$remainder, this_bill=$it")
+            if (remainder >= it.faceValue) {
+                val billQuantity = remainder / it.faceValue
+                result.merge(it, billQuantity) { old, new -> old + new }
+                remainder -= it.faceValue * result.getOrDefault(it, 0)
             }
-            result
         }
     }
 
     override fun sumEvenNumbers(numbersSequence: List<Int>): Int {
-        return numbersSequence.filter { it % 2 == 0 }.sum()
-    }
-
-    private fun memoizedRecursiveFibonacci(n: Int, cache: MutableMap<Int, Long>): Long {
-        return when {
-            n <= 0 -> {
-                cache[n] = 0
-                0
-            }
-            n <= 2 -> {
-                cache[n] = 1
-                1
-            }
-            else -> {
-                val calculated = (memoizedRecursiveFibonacci(n - 1, cache)
-                        + memoizedRecursiveFibonacci(n - 2, cache))
-                cache[n] = calculated
-                calculated
-            }
+        log.info("I=init, numbersSequence=$numbersSequence")
+        return numbersSequence.filter { it % 2 == 0 }.sum().also {
+            log.info("I=finish, result=$it")
         }
     }
 }
